796455ea6f4ea123b06123ebe68af3a6886c76a8
dvthnight <94679147+dvthnight@users.noreply.github.com>
2022-04-21 18:10:18 +0200

    creato file model

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     public function index(){
 
-        $movies = [];
+        $movies = Movie::all();
 
         return view("home", compact("movies"));
     }

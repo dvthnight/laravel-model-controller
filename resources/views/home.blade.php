f481eee555c54377266f6e95e295888650636c04
dvthnight <94679147+dvthnight@users.noreply.github.com>
2022-04-21 19:21:05 +0200

    aggiunto stile scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,17 @@
     <main>
         <h2>Film Card</h2>
         
-        @foreach ($movies as $movie)
+        <div class="container">
+            @foreach ($movies as $movie)
             <ul class="card">
-                <li>{{$movie->title}}</li>
-                <li>{{$movie->original_title}}</li>
-                <li>{{$movie->nationality}}</li>
-                <li>{{$movie->date}}</li>
-                <li>{{$movie->vote}}</li>
+                <li><h4>Titolo:</h4> {{$movie->title}}</li>
+                <li><h4>Titolo originale:</h4> {{$movie->original_title}}</li>
+                <li><h4>Nazionalità:</h4> {{$movie->nationality}}</li>
+                <li><h4>Data uscita:</h4> {{$movie->date}}</li>
+                <li><h4>Voto:</h4> {{$movie->vote}}</li>
             </ul>
         @endforeach
+        </div>
         
     </main>
 @endsection

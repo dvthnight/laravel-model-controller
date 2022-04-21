ab0addb28034eb190decc9b5ccc517eb9fc7349c
dvthnight <94679147+dvthnight@users.noreply.github.com>
2022-04-21 18:33:03 +0200

    stampo elementi movie

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Movies</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.standard')
+@section('metaTitle')
+    Movies
+@endsection
+@section('content')
+    <main>
+        <h2>Film Card</h2>
+        
+        @foreach ($movies as $movie)
+            <ul class="card">
+                <li>{{$movie->title}}</li>
+                <li>{{$movie->original_title}}</li>
+                <li>{{$movie->nationality}}</li>
+                <li>{{$movie->date}}</li>
+                <li>{{$movie->vote}}</li>
+            </ul>
+        @endforeach
+        
+    </main>
+@endsection
